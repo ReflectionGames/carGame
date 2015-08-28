df29a0495638c1f28a8f6dedb37e9beee93c833e
Brainiac98 <njlh98@icloud.com>
2015-08-28 17:11:19 -0500

    Added a menu/map select

---
+++ unityProject/Assets/Select Map Script.js
@@ -0,0 +1,18 @@
+﻿var levelToLoad : int;
+var soundhover : AudioClip;
+var beep : AudioClip;
+var QuitButton=false;
+function OnMouseEnter(){
+GetComponent.<AudioSource>().PlayOneShot(soundhover);
+}
+function OnMouseUp(){
+GetComponent.<AudioSource>().PlayOneShot(beep);
+yield new WaitForSeconds(0.35);
+if(QuitButton){
+Application.Quit();
+}
+else{
+Application.LoadLevel(levelToLoad);
+}
+}
+@script RequireComponent(AudioSource)
