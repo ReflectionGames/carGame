55c88a2580f99244c3a5135381b45837d33c06b7
rdfriedl <rdfriedl@gmail.com>
2015-08-29 19:07:22 -0500

    added UI sound FX

---
+++ unityProject/Assets/scripts/SoundPlayer.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var Sounds: sound[];
+private var audioSources: Hashtable = new Hashtable();
+
+function Start () {
+	for(var i: int = 0; i < Sounds.Length; i++){
+		var source: AudioSource = gameObject.AddComponent.<AudioSource>();
+		source.loop = false;
+		source.clip = Sounds[i].sound;
+		audioSources.Add(Sounds[i].name,source);
+	}
+}
+
+function PlaySound (sound: String) {
+	if(audioSources.ContainsKey(sound)){
+		var audio: AudioSource = audioSources[sound] as AudioSource;
+		audio.Play();
+	}
+}
+
+class sound{
+	public var name: String;
+	public var sound: AudioClip;
+}
--- unityProject/Assets/scripts/car/lightControler.js
+++ unityProject/Assets/scripts/car/lightControler.js
@@ -3,15 +3,25 @@
 import UnityStandardAssets.CrossPlatformInput;
 
 var InputName: String;
+var Sound: AudioClip;
 var lights: Light[];
 private var lightsOn: boolean = false;
 private var val: boolean = false;
+private var lightsSound: AudioSource;
+
+function Start(){
+	if(Sound){
+		lightsSound = gameObject.AddComponent.<AudioSource>();
+		lightsSound.clip = Sound;
+	}
+}
 
 function Update () {
 	//test for user input
 	if(CrossPlatformInputManager.GetButtonDown(InputName)){
 		//toggle lights
 		lightsOn = !val;
+		if(lightsSound) lightsSound.Play();
 	}
 	else if(val != lightsOn){
 		val = lightsOn;
