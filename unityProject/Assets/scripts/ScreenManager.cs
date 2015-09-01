eb59e8212a3c85dae55266dddc1c5ce5d93bfc48
rdfriedl <rdfriedl@gmail.com>
2015-09-01 18:12:48 -0500

    added "Options" to menu
    contains mute check box and volume slider

--- unityProject/Assets/scripts/ScreenManager.cs
+++ unityProject/Assets/scripts/ScreenManager.cs
@@ -8,6 +8,8 @@ public class ScreenManager : MonoBehaviour {
 	
 	//Screen to open automatically at the start of the Scene
 	public Animator initiallyOpen;
+
+	public menuSetting[] Menus;
 	
 	//Currently Open Screen
 	private Animator m_Open;
@@ -15,23 +17,23 @@ public class ScreenManager : MonoBehaviour {
 	//Hash of the parameter we use to control the transitions.
 	private int m_OpenParameterId;
 	
-	//The GameObject Selected before we opened the current Screen.
-	//Used when closing a Screen, so we can go back to the button that opened it.
-	private GameObject m_PreviouslySelected;
-	
 	//Animator State and Transition names we need to check against.
 	const string k_OpenTransitionName = "Open";
 	const string k_ClosedStateName = "Closed";
 	
+	const string closedStateName = "Closed";
+	const string openBool = "openBool";
+	private int openBoolHash = 0;
+	
 	public void OnEnable()
 	{
-		//We cache the Hash to the "Open" Parameter, so we can feed to Animator.SetBool.
 		m_OpenParameterId = Animator.StringToHash (k_OpenTransitionName);
+		openBoolHash = Animator.StringToHash (openBool);
 		
 		//If set, open the initial Screen now.
-		if (initiallyOpen == null)
-			return;
-		OpenPanel(initiallyOpen);
+		if (initiallyOpen) {
+			OpenPanel(initiallyOpen);
+		}
 	}
 	
 	//Closes the currently open panel and opens the provided one.
@@ -43,15 +45,11 @@ public class ScreenManager : MonoBehaviour {
 		
 		//Activate the new Screen hierarchy so we can animate it.
 		anim.gameObject.SetActive(true);
-		//Save the currently selected button that was used to open this Screen. (CloseCurrent will modify it)
-		var newPreviouslySelected = EventSystem.current.currentSelectedGameObject;
 		//Move the Screen to front.
 		anim.transform.SetAsLastSibling();
 
 		//close
 		CloseCurrent ();
-		
-		m_PreviouslySelected = newPreviouslySelected;
 		
 		//Set the new Screen as then open one.
 		m_Open = anim;
@@ -62,19 +60,55 @@ public class ScreenManager : MonoBehaviour {
 		//GameObject go = FindFirstEnabledSelectable(anim.gameObject);
 		//SetSelected(go);
 	}
-	
-	//Finds the first Selectable element in the providade hierarchy.
-	static GameObject FindFirstEnabledSelectable (GameObject gameObject)
-	{
-		GameObject go = null;
-		var selectables = gameObject.GetComponentsInChildren<Selectable> (true);
-		foreach (var selectable in selectables) {
-			if (selectable.IsActive () && selectable.IsInteractable ()) {
-				go = selectable.gameObject;
-				break;
+
+	public void OpenMenu (string name){
+		for (int i = 0; i < Menus.Length; i++) {
+			if(Menus[i].name == name){
+				OpenMenu(Menus[i].menu);
+				return;
 			}
 		}
-		return go;
+		Debug.LogError ("Failed to open menu: "+name);
+	}
+
+	public void OpenMenu (Animator anim){
+		//close all other menus
+		closeAllMenusBut (anim);
+
+		//start anim
+		anim.gameObject.SetActive (true);
+		anim.SetBool (this.openBoolHash,true);
+	}
+
+	public void closeAllMenus(){
+		for(int i = 0; i < Menus.Length; i++){
+			if(isOpen(Menus[i].menu)){
+				closeMenu(Menus[i].menu);
+			}
+		}
+	}
+
+	public void closeAllMenusBut(Animator anim){
+		for(int i = 0; i < Menus.Length; i++){
+			if(isOpen(Menus[i].menu) && Menus[i].menu != anim){
+				closeMenu(Menus[i].menu);
+			}
+		}
+	}
+
+	public void closeMenu(Animator anim){
+		anim.SetBool (openBoolHash,false);
+		StartCoroutine (DisableDeleyed(anim));
+	}
+
+	private bool isOpen(Animator anim){
+		if (!anim.IsInTransition (0))
+			return true;
+		return anim.GetCurrentAnimatorStateInfo (0).IsName (closedStateName);
+	}
+
+	private bool isClosed(Animator anim){
+		return !isOpen (anim);
 	}
 	
 	//Closes the currently open Screen
@@ -87,18 +121,14 @@ public class ScreenManager : MonoBehaviour {
 		
 		//Start the close animation.
 		m_Open.SetBool(m_OpenParameterId, false);
-		
-		//Reverting selection to the Selectable used before opening the current screen.
-		//SetSelected(m_PreviouslySelected);
+
 		//Start Coroutine to disable the hierarchy when closing animation finishes.
-		StartCoroutine(DisablePanelDeleyed(m_Open));
+		StartCoroutine(DisableDeleyed(m_Open));
 		//No screen open.
 		m_Open = null;
 	}
-	
-	//Coroutine that will detect when the Closing animation is finished and it will deactivate the
-	//hierarchy.
-	IEnumerator DisablePanelDeleyed(Animator anim)
+
+	IEnumerator DisableDeleyed(Animator anim)
 	{
 		bool closedStateReached = false;
 		bool wantToClose = true;
@@ -115,23 +145,10 @@ public class ScreenManager : MonoBehaviour {
 		if (wantToClose)
 			anim.gameObject.SetActive(false);
 	}
-	
-	//Make the provided GameObject selected
-	//When using the mouse/touch we actually want to set it as the previously selected and 
-	//set nothing as selected for now.
-	private void SetSelected(GameObject go)
-	{
-		//Select the GameObject.
-		EventSystem.current.SetSelectedGameObject(go);
-		
-		//If we are using the keyboard right now, that's all we need to do.
-		var standaloneInputModule = EventSystem.current.currentInputModule as StandaloneInputModule;
-		if (standaloneInputModule != null && standaloneInputModule.inputMode == StandaloneInputModule.InputMode.Buttons)
-			return;
-		
-		//Since we are using a pointer device, we don't want anything selected. 
-		//But if the user switches to the keyboard, we want to start the navigation from the provided game object.
-		//So here we set the current Selected to null, so the provided gameObject becomes the Last Selected in the EventSystem.
-		EventSystem.current.SetSelectedGameObject(null);
-	}
+}
+
+[System.Serializable]
+public class menuSetting{
+	public string name;
+	public Animator menu;
 }
---
+++ unityProject/Assets/scripts/Singleton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Singleton : MonoBehaviour {
+	private static Singleton instance = null;
+	public static Singleton Instance {
+		get { return instance; }
+	}
+	public void Awake() {
+		if (instance != null && instance != this){
+			Destroy(this.gameObject);
+			return;
+		} 
+		else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
