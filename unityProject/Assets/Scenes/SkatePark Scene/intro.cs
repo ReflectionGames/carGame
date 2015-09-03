cf00d67123692b82e9da3419b50191f427be4597
rdfriedl <rdfriedl@gmail.com>
2015-09-03 13:28:32 -0500

    added intro to SkatePark
    added ramp3
    removed fog from all scenes
    added SkatePark to main menu

---
+++ unityProject/Assets/Scenes/SkatePark Scene/intro.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class intro : MonoBehaviour {
+	public void Stop () {
+		Animator anim = gameObject.GetComponent<Animator> ();
+		anim.enabled = false;
+	}
+}
